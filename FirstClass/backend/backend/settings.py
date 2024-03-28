866fc4399192ddeff1ced45c3f074ea57c7136d0
Ivan <saintsrowm0nkrus@gmail.com>
2024-03-28 16:48:39 +0500

    создал приложение "landing" добвил пакет "psycopg" для работы с psql создал БД "users_info"

--- FirstClass/backend/backend/settings.py
+++ FirstClass/backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users_info',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
