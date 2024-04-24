815326cf2f15e60c05f8a68eb78523a6c46c5993
hydregnej <bannito74@gmail.com>
2024-04-24 18:51:51 +0500

    not back

--- FirstClass/django_docker/landing/enquires/urls.py
+++
@@ -1,5 +0,0 @@
-from .views import GuestAPIList
-
-urlpatterns = [
-    path('enquires', GuestAPIList.as_view())
-]
--- FirstClass/django_docker/landing/enquires/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Enquires
-from .serializer import GuestSerializer
-
-class EnquiresAPIList(generics.CreateAPIView):
-    queryset = Enquires.objects.all()
-    serializer_class = GuestSerializer
