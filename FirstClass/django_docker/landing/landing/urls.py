68e703e9466872fc5d60f9e9cdf6d3fae85d2ff1
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-07-20 03:57:04 +0500

    delete backend

--- FirstClass/django_docker/landing/landing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from enquires.views import EnquiresAPIList
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('enquires/', EnquiresAPIList.as_view()),
-]
