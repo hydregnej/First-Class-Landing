9da0c8899573b8061d962eb3adb9563795aaa305
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-07-26 02:19:29 +0500

    Заменил лого на меньший размер, подправил адаптив

--- js/slider.js
+++ js/slider.js
@@ -9,6 +9,19 @@ const swiperTravelExamples = new Swiper(".travel-examples-slider-swiper", {
     nextEl: ".travel-examples-slider__next",
     prevEl: ".travel-examples-slider__prev",
   },
+  breakpoints: {
+    // mobile + tablet - 320-990
+    320: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    // desktop >= 991
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 19
+    }
+  }
+  
 });
 
 const swiperReviews = new Swiper(".reviews-slider__swiper", {
